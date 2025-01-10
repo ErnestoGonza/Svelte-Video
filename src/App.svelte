2e02c459a07e9485685b922ee35ede3f9b549095
Ernesto Gonzalez <ERNESTOGONZALEZ@hubgroup.com>
2025-01-10 13:38:59 -0500

    Adds mux video

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,26 @@
 <script>
 import { Video, Card } from 'flowbite-svelte';
+import '@mux/mux-video';
 </script>
 
 <main class="h-screen">
-  <div class="flex items-start h-full m-4">
+  <div class="flex items-center h-full m-0">
     <!-- Video Section -->
     <div class="w-3/4 h-full flex items-center justify-center flex-col">
-      <Video src="/assets/boat.mp4" controls class="w-full h-3/4 rounded-lg"></Video>
-      <div class="m-4 w-[90%] h-40 bg-gray-800 text-white border border-gray-700 rounded-lg shadow-lg flex justify-center items-center">
+      <!-- <Video src="/assets/boat.mp4" controls class="w-full h-3/4 rounded-lg"></Video> -->
+      <mux-video
+        playback-id="DS00Spx1CV902MCtPj5WknGlR102V5HFkDe"
+        metadata-video-title="Max Max Fury Road"
+        metadata-viewer-user-id="user-id-1234"
+        controls
+      ></mux-video>
+      <div class="m-4 w-[90%] h-32 bg-gray-800 text-white border border-gray-700 rounded-lg shadow-lg flex justify-center items-center">
         Play <!--TODO Create Play Button Card-->
       </div>
     </div>
 
     <!-- Data Section -->
-    <div class="m-4 w-1/4 h-5/6 bg-gray-800 text-white border border-gray-700 rounded-lg shadow-lg">
+    <div class="m-4 w-1/4 h-3/4 bg-gray-800 text-white border border-gray-700 rounded-lg shadow-lg">
       Data
     </div>
   </div>
